2c413a4aa10b9bc1a3eb20f2334aa0d969c2641f
Ashley Owens <ash4741@gmail.com>
2019-03-09 13:27:06 -0500

    Add Get By ID to Action Router
    also corrected some variable naming

--- data/helpers/actionModel_router.js
+++ data/helpers/actionModel_router.js
@@ -6,8 +6,18 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const project = await Actions.get();
-    res.status(200).json(project);
+    const action = await Actions.get();
+    res.status(200).json(action);
+  }
+  catch (error) {
+    res.status(500).json({error: "Error retrieving data"})
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try{
+    const action = await Actions.get(req.params.id);
+    res.status(200).json(action);
   }
   catch (error) {
     res.status(500).json({error: "Error retrieving data"})
