b5b57f1ce09ef512b6b49d6ececaa440440f9bfc
Ashley Owens <ash4741@gmail.com>
2019-03-09 13:30:02 -0500

    Add Get By ID to Project Router

--- data/helpers/projectModel_router.js
+++ data/helpers/projectModel_router.js
@@ -14,4 +14,14 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try{
+    const project = await Projects.get(req.params.id);
+    res.status(200).json(project);
+  }
+  catch (error) {
+    res.status(500).json({error: "Error retrieving data"})
+  }
+});
+
 module.exports = router;
