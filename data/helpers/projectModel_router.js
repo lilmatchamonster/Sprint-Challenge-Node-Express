efb4091abc6884b26091e719b9a086d821461640
Ashley Owens <ash4741@gmail.com>
2019-03-09 13:37:38 -0500

    Add Put to Project Router

--- data/helpers/projectModel_router.js
+++ data/helpers/projectModel_router.js
@@ -15,12 +15,22 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  try{
+  try {
     const project = await Projects.get(req.params.id);
     res.status(200).json(project);
   }
   catch (error) {
     res.status(500).json({error: "Error retrieving data"})
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updated = await Projects.update(req.params.id, req.body);
+    res.status(200).json(updated);
+  }
+  catch (error) {
+    res.status(500).json({error: "Error updating data"})
   }
 });
 
