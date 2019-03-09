fdc073253185e7059f31b32db39bfa50111a907c
Ashley Owens <ash4741@gmail.com>
2019-03-09 13:21:46 -0500

    Add Get to Project Router

---
+++ data/helpers/projectModel_router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const Projects = require('./projectModel.js');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const project = await Projects.get();
+    res.status(200).json(project);
+  }
+  catch (error) {
+    res.status(500).json({error: "Error retrieving data"})
+  }
+});
+
+module.exports = router;
