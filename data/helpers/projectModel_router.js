e1b1255d0667496cf0548e51c6668e87ee5a14f9
Ashley Owens <ash4741@gmail.com>
2019-03-09 13:44:43 -0500

    Add Delete to Project Router

--- data/helpers/projectModel_router.js
+++ data/helpers/projectModel_router.js
@@ -34,4 +34,14 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const project = await Projects.remove(req.params.id);
+    res.status(200).json({message: "Project has been removed"})
+  }
+  catch (error) {
+    res.status(500).json({error: "Error removing data"})
+  }
+});
+
 module.exports = router;
